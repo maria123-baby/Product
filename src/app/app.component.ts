44250e97ad57a4e719d434cd32d109e908c414ab
maria123-baby <145011381+maria123-baby@users.noreply.github.com>
2024-02-09 09:32:11 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
+import { SlicePipe } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SlicePipe,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'products';
+  constructor(public api:ApiService){}
+  ngOnInit(){
+    this.api.getData().subscribe(response=>{this.data=response})
+  }
+  data:any=[]
+    
+
 }
